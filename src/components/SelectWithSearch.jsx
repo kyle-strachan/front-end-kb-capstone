f49d5c676141cff3e40513816f6ae11bdac04cbb
reiteach <kylestrachan@outlook.com>
2025-11-12 16:23:34 -0800

    Init

---
+++ src/components/SelectWithSearch.jsx
@@ -0,0 +1,26 @@
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+
+export default function SelectWithSearch({
+    options = [],
+    value,
+    onChange,
+    label = "Select",
+    labelField = "name",
+    multiple = false,
+}) {
+    return (
+        <Autocomplete
+            multiple={multiple}
+            options={options}
+            getOptionLabel={(option) => option?.[labelField] || ""}
+            value={value}
+            onChange={onChange}
+            renderInput={(params) => (
+                <TextField {...params} label={label} placeholder="Search..." variant="standard" />
+            )}
+            size="small"
+            fullWidth
+        />
+    );
+}
