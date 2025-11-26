8572c20cfae1b3dd9269400df47f67c40db0a399
reiteach <kylestrachan@outlook.com>
2025-11-25 17:33:06 -0800

    Account and change password functions implemented

---
+++ src/components/InputTextPassword.jsx
@@ -0,0 +1,57 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import IconButton from '@mui/material/IconButton';
+import Input from '@mui/material/Input';
+import FilledInput from '@mui/material/FilledInput';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormHelperText from '@mui/material/FormHelperText';
+import FormControl from '@mui/material/FormControl';
+import TextField from '@mui/material/TextField';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+
+export default function InputTextPassword({ value, label = "Password" }) {
+    const [showPassword, setShowPassword] = React.useState(false);
+    const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+    const handleMouseDownPassword = (event) => {
+        event.preventDefault();
+    };
+
+    const handleMouseUpPassword = (event) => {
+        event.preventDefault();
+    };
+
+    return (
+        <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
+            <div>
+                <FormControl sx={{ mb: 2, width: '30ch' }} variant="outlined">
+                    <InputLabel htmlFor="outlined-adornment-password">{label}</InputLabel>
+                    <OutlinedInput
+                        id="outlined-adornment-password"
+                        type={showPassword ? 'text' : 'password'}
+                        onChange={value}
+                        endAdornment={
+                            <InputAdornment position="end">
+                                <IconButton
+                                    aria-label={
+                                        showPassword ? 'hide the password' : 'display the password'
+                                    }
+                                    onClick={handleClickShowPassword}
+                                    onMouseDown={handleMouseDownPassword}
+                                    onMouseUp={handleMouseUpPassword}
+                                    edge="end"
+                                >
+                                    {showPassword ? <VisibilityOff /> : <Visibility />}
+                                </IconButton>
+                            </InputAdornment>
+                        }
+                        label={label}
+                    />
+                </FormControl>
+            </div>
+        </Box>
+    );
+}
--- src/pages/Account.jsx
+++ src/pages/Account.jsx
@@ -1,14 +1,80 @@
 import { useAuth } from "../context/AuthContext";
+import { Paper, Typography, Button, TextField } from "@mui/material"
+import InputTextPassword from "../components/InputTextPassword"
+import { useState } from "react";
+import { useEffect } from "react";
+import { api } from "../api";
+import { ToastContainer, toast } from "react-toastify";
 
-export default function Dashboard() {
+export default function Account() {
     const { user } = useAuth();
-    return (
-        <div>
-            <h2>Account</h2>
-            <p>Welcome {user?.fullName || user?.username}</p>
-            <p>{user?.department}</p>
-            <p>{user?.passwordHash}</p>
+    const [currentPassword, setCurrentPassword] = useState("");
+    const [newPassword, setNewPassword] = useState("");
+    const [newPassword2, setNewPassword2] = useState("");
+    // const [passwordMatch, setPasswordMatch] = useState(false);
 
-        </div>
+    function notify(message, type = "Info") {
+        if (type === "success") {
+            toast.success(message);
+        } else {
+            toast.error(message);
+        }
+    }
+
+    async function handleUpdatePassword() {
+        try {
+            const res = await api.patch("/auth/change-password");
+            if (res.status === 200) {
+                notify("Password changed successfully.", "success");
+            } else {
+                notify(res.message, "error");
+            }
+        } catch (error) {
+            const message =
+                error.response?.data?.message || error.message || "Unknown error";
+            notify(message, "error");
+        }
+    }
+
+    // useEffect(() => {
+    //     if (newPassword === newPassword2) {
+    //         setPasswordMatch(true);
+    //     } else {
+    //         setPasswordMatch(false);
+    //     }
+    // }, [newPassword, newPassword2]);
+
+    return (
+        <>
+            <div style={{ margin: "0 auto", padding: "1rem" }}>
+                <Paper sx={{ p: 3, mb: "1rem" }}>
+                    <Typography variant="h5" sx={{ mb: 2 }}>
+                        My Account
+                    </Typography>
+                    <Typography variant="h6" sx={{ mb: 2 }}>
+                        {user.fullName}
+                    </Typography>
+                    <Typography sx={{ mb: 2 }}>Username: {user.username}</Typography>
+                    <Typography sx={{ mb: 2 }}>Position: {user.postion}</Typography>
+                    <Typography sx={{ mb: 2 }}>Email: {user.email}</Typography>
+                    <Typography sx={{ mb: 2 }}>Assigned Roles: {user.roles}</Typography>
+                </Paper>
+                <Paper sx={{ p: 3 }}>
+                    <Typography variant="h6" sx={{ mb: 2 }}>
+                        Change Password
+                    </Typography>
+                    <InputTextPassword value={(e) => setCurrentPassword(e.target.value)} label="Current Password" />
+                    <InputTextPassword value={(e) => setNewPassword(e.target.value)} label="New Password" />
+                    <InputTextPassword value={(e) => setNewPassword2(e.target.value)} label="Confirm New Password" />
+                    {newPassword2.length > 0 && newPassword !== newPassword2 && (
+                        <Typography color="error" sx={{ mb: 2 }}>New passwords do not match.</Typography>
+                    )}
+                    <Button variant="contained" onClick={handleUpdatePassword} disabled={!(currentPassword.length > 0 && newPassword.length > 0 && newPassword2.length > 0 && newPassword === newPassword2)} >
+                        Change Password
+                    </Button>
+                </Paper>
+            </div>
+            <ToastContainer />
+        </>
     );
 }
