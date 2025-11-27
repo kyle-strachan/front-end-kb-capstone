4adceb1f6cde1d99a883466d09ad3ea376fcc318
reiteach <kylestrachan@outlook.com>
2025-11-26 17:06:46 -0800

    Dashboard placeholders

---
+++ src/components/PlaceholderAmenityRequest.jsx
@@ -0,0 +1,11 @@
+import { Typography, Paper } from "@mui/material";
+
+export default function PlaceholderAmenityRequest() {
+
+    return (
+        <Paper sx={{ width: "200px", height: "200px" }}>
+            <Typography>Amenity</Typography>
+
+        </Paper>
+    )
+}
--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -6,12 +6,13 @@ import Paper from "@mui/material/Paper";
 import DocTree from "../components/DocTree";
 import RequestsOverview from '../components/RequestsOverview';
 import DocSearch from '../components/DocSearchBox';
+import PlaceholderAmenityRequest from '../components/PlaceholderAmenityRequest';
 
 export default function Dashboard() {
     return (
         <div style={{ maxWidth: "1280px", height: "vh", margin: "0 auto", padding: "1rem" }}>
-            <h2>Dashboard</h2>
-            <div style={{ display: "flex", gap: "1rem", width: "100%" }}>
+            {/* <h2>Dashboard</h2> */}
+            <div style={{ display: "flex", flexDirection: "row", gap: "1rem", width: "100%" }}>
                 <div>
                     <Paper style={{ width: "300px" }}>
                         <DocSearch />
@@ -20,8 +21,23 @@ export default function Dashboard() {
                         <DocTree />
                     </Paper>
                 </div>
-                <div style={{ width: "100%" }}>
-                    <RequestsOverview />
+                <div style={{ display: "flex", flexDirection: "column", gap: "1rem", width: "100%" }}>
+
+                    <div style={{ width: "100%" }}>
+                        <RequestsOverview />
+                    </div>
+                    <div style={{
+                        display: "grid",
+                        gridTemplateColumns: "repeat(auto-fill, minmax(200px, 1fr))",
+                        gap: "1rem"
+                    }}>
+                        <PlaceholderAmenityRequest />
+                        <PlaceholderAmenityRequest />
+                        <PlaceholderAmenityRequest />
+                        <PlaceholderAmenityRequest />
+                        <PlaceholderAmenityRequest />
+                        <PlaceholderAmenityRequest />
+                    </div>
                 </div>
             </div>
         </div>
