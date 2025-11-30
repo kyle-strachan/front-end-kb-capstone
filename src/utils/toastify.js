4f11bdef775d75ca835985895c2e2f860e445b3d
reiteach <kylestrachan@outlook.com>
2025-11-30 13:55:25 -0800

    Shortened autoclose windows by type

--- src/utils/toastify.js
+++ src/utils/toastify.js
@@ -3,17 +3,17 @@ import { toast } from "react-toastify";
 export default function notify(message, type = "info") {
   switch (type.toLowerCase()) {
     case "success":
-      toast.success(message);
+      toast.success(message, { autoClose: 2000 });
       break;
     case "error":
-      toast.error(message);
+      toast.error(message, { autoClose: 5000 });
       break;
     case "warn":
-      toast.warn(message);
+      toast.warn(message, { autoClose: 3000 });
       break;
     case "info":
     default:
-      toast.info(message);
+      toast.info(message, { autoClose: 3000 });
       break;
   }
 }
